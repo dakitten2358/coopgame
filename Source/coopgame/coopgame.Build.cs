953fb0c807e55f21b11c7023626943b7ac6405e3
dakitten2358 <jay.w.murray@gmail.com>
2017-04-05 20:09:58 -0400

    Revert "Merge pull request #1 from dakitten2358/steam.support"
    This reverts commit 72e218a66ad287cbfd8c5a6d6eefa4e7f778a570, reversing
    changes made to 5d6676a9c5798b2e47b3ef59ebfe068485c6af8d.
    
    Conflicts:
    	Config/DefaultEngine.ini

--- Source/coopgame/coopgame.Build.cs
+++ Source/coopgame/coopgame.Build.cs
@@ -8,6 +8,5 @@ public class coopgame : ModuleRules
 	{
 		Definitions.Add("_CRT_SECURE_NO_WARNINGS");
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-        DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 	}
 }

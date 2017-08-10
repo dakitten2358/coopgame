310929e906ff0cce14336611621f932a0c2d9fe7
dakitten2358 <jay.w.murray@gmail.com>
2017-08-10 18:31:24 -0400

    fix spawning of enemy from task

--- Source/coopgame/director/BTT_AD_SpawnEnemy.cpp
+++ Source/coopgame/director/BTT_AD_SpawnEnemy.cpp
@@ -3,13 +3,17 @@
 #include "coopgame.h"
 #include "BTT_AD_SpawnEnemy.h"
 #include "NativeCoopGameMode.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
 
-void UBTT_AD_SpawnEnemy::OnGameplayTaskActivated(class UGameplayTask& Task)
+EBTNodeResult::Type UBTT_AD_SpawnEnemy::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	auto gameMode = GetWorld()->GetAuthGameMode<ANativeCoopGameMode>();
 	if (gameMode)
 	{
-		UE_LOG(LogCoopGame, Warning, TEXT("UBTT_AD_SpawnEnemy::OnGameplayTaskActivated: Spawning new enemy"));	
-		gameMode->SpawnNewEnemy();		
+		UE_LOG(LogCoopGame, Warning, TEXT("UBTT_AD_SpawnEnemy::ExecuteTask: Spawning new enemy"));
+		gameMode->SpawnNewEnemy();
+
+		return EBTNodeResult::Succeeded;
 	}
+	return EBTNodeResult::Failed;
 }
--- Source/coopgame/director/BTT_AD_SpawnEnemy.h
+++ Source/coopgame/director/BTT_AD_SpawnEnemy.h
@@ -4,7 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/BehaviorTreeTypes.h"
 #include "BTT_AD_SpawnEnemy.generated.h"
+
 
 /**
  * 
@@ -15,7 +17,7 @@ class COOPGAME_API UBTT_AD_SpawnEnemy : public UBTTaskNode
 	GENERATED_BODY()
 	
 public:
-	virtual void OnGameplayTaskActivated(class UGameplayTask& Task) override;
+	virtual EBTNodeResult::Type ExecuteTask(class UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
 	
 };
